e564c6dca4eadf4ba1e3b0e6bfcbfd3f451db27e
MdRiabi <mhdriabi2013@gmail.com>
2023-02-26 12:22:55 +0100

    JobTitle Repository

---
+++ src/main/java/com/RiabyTheGenuis/fleetapp/repositories/JobTitleRepository.java
@@ -0,0 +1,7 @@
+package com.RiabyTheGenuis.fleetapp.repositories;
+
+import com.RiabyTheGenuis.fleetapp.models.JobTitle;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JobTitleRepository extends JpaRepository<JobTitle , Integer> {
+}
